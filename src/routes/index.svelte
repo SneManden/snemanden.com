3d0b020e2fda0ad312ac132775bc0483236d55d9
Casper Kehlet Jensen <casper@snemanden.com>
2020-08-12 21:29:53 +0200

    degit repository + remove graphql

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ExampleComponent from "../components/ExampleComponent.svelte";
+</script>
+
+<style>
+	.centerer {
+		flex: 1 1 0%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
+
+<div class="centerer">
+	<ExampleComponent
+		title="🌐 Sapper with TypeScript project base"
+		paragraph="This is an example route and component to make sure everything's working." />
+</div>
