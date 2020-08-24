fbb9b733d9399297ee4b88a7f60389ab089add31
Casper Kehlet Jensen <casper@snemanden.com>
2020-08-24 21:46:13 +0200

    Ordering of posts

--- src/routes/arkiv/_posts.ts
+++ src/routes/arkiv/_posts.ts
@@ -9,5 +9,6 @@ export const getArchivedPosts = () => {
     return fs
         .readdirSync(dir)
         .filter(file => path.extname(file) === ".md")
-        .map<IPost>(file => parseFile(dir, file));
+        .map<IPost>(file => parseFile(dir, file))
+        .sort((a, b) => b.published.timestamp.localeCompare(a.published.timestamp));
 };
